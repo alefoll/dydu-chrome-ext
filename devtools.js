7676e38f7afdf5076991321a5e8e76eabfd9bb84
Alexandre Le Foll <alefoll@doyoudreamup.com>
2016-12-20 14:37:54 +0100

    It's working!

--- devtools.js
+++ devtools.js
@@ -1,1 +1,48 @@
-chrome.devtools.panels.create('DYDU', null, 'panel.html');
+const history = [];
+
+// function requestHandler(har_entry) {
+//     const url = document.createElement("a");
+
+//     url.href = har_entry.request.url;
+
+//     if (url.pathname.substr(url.pathname.length - 8) === "chatHttp" && url.search.length > 1) {
+//         const parameters = {};
+//         const request    = {}
+
+//         for (let i = 0, params = url.search.substr(1).split("&"); i < params.length; i++) {
+//             let separator = params[i].indexOf("=");
+
+//             let param = params[i].slice(0, separator);
+//             let data  = params[i].slice(separator + 1);
+
+//             parameters[decodeURIComponent(param)] = data.length > 1 ? decodeURIComponent(data) : "";
+//         }
+
+//         request.sent = JSON.parse(parameters.data);
+
+//         if (har_entry.response.status >= 200 && har_entry.response.status < 300 && /^dydu\.(.*)\.callbacks\._([0-9]*)\(/.test(har_entry.response.content))
+//             request.response = JSON.parse(har_entry.response.content.replace(/^dydu\.(.*)\.callbacks\._([0-9]*)\(/, "").slice(0, -1));
+
+//         history.push(request);
+//     }
+// }
+
+chrome.devtools.panels.create('DYDU', null, 'panel.html', (extensionPanel) => {
+    let _window; // Going to hold the reference to panel.html's `window`
+
+    extensionPanel.onShown.addListener(function temp(panelWindow) {
+        extensionPanel.onShown.removeListener(temp); // Run once only
+
+        // chrome.devtools.network.onRequestFinished.removeListener(requestHandler);
+
+        _window = panelWindow;
+
+        // Release queued data
+        let message;
+
+        while (message = history.shift())
+            _window.requestHandler(message);
+    });
+});
+
+// chrome.devtools.network.onRequestFinished.addListener(requestHandler);
--- panel.js
+++ panel.js
@@ -1,7 +1,36 @@
-const history = [];
-const url     = document.createElement("a");
+let history = 1;
 
-let editor;
+let editorSent;
+let editorResponse;
+
+function updateMonaco(sent, response) {
+    sent     = JSON.stringify(sent,     null, 4);
+    response = JSON.stringify(response, null, 4);
+
+    if (editorSent === undefined) {
+        editorSent = monaco.editor.create(document.querySelector("#editorSent"), {
+            language             : "json",
+            readOnly             : true,
+            scrollBeyondLastLine : false,
+            theme                : "vs-dark",
+            value                : sent
+        });
+    } else {
+        editorSent.setValue(sent);
+    }
+
+    if (editorResponse === undefined) {
+        editorResponse = monaco.editor.create(document.querySelector("#editorResponse"), {
+            language             : "json",
+            readOnly             : true,
+            scrollBeyondLastLine : false,
+            theme                : "vs-dark",
+            value                : response
+        });
+    } else {
+        editorResponse.setValue(response);
+    }
+}
 
 function decode(data) {
     for (let prop in data) {
@@ -10,31 +39,14 @@ function decode(data) {
         else if (/^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/.test(data[prop]))
             data[prop] = Base64.decode(data[prop]);
     }
+
+    return data;
 }
-
-function updateMonaco(value) {
-    value = JSON.stringify(value, null, 4);
-
-    if (editor === undefined) {
-        editor = monaco.editor.create(document.querySelector("#container"), {
-            language             : "json",
-            readOnly             : true,
-            scrollBeyondLastLine : false,
-            theme                : "vs-dark",
-            value                : value
-        });
-    } else {
-        editor.setValue(value);
-    }
-}
-
-chrome.devtools.panels.sources.createSidebarPane('test', function(sidebar) {
-        // sidebar initialization code here
-        sidebar.setObject({ some_data: "Some data to show" });
-});
 
 chrome.devtools.network.onRequestFinished.addListener((har_entry) => {
-    url.href = har_entry.request.url
+    const url = document.createElement("a");
+
+    url.href = har_entry.request.url;
 
     if (url.pathname.substr(url.pathname.length - 8) === "chatHttp" && url.search.length > 1) {
         const parameters = {};
@@ -48,22 +60,34 @@ chrome.devtools.network.onRequestFinished.addListener((har_entry) => {
             parameters[decodeURIComponent(param)] = data.length > 1 ? decodeURIComponent(data) : "";
         }
 
-        const data = JSON.parse(parameters.data);
-
-        decode(data);
-
-        history.push(data);
-
-        const button = document.createElement('button');
-
-        button.innerText = history.length;
-
-        button.addEventListener('click', () => {
-            updateMonaco(data);
-        }, false);
-
-        document.querySelector("#history").appendChild(button);
-
-        updateMonaco(data)
+        har_entry.getContent(function(content) {
+            if (har_entry.response.status >= 200 && har_entry.response.status < 300 && /^dydu\.(.*)\.callbacks\._([0-9]*)\(/.test(content)) {
+                requestHandler(JSON.parse(parameters.data), JSON.parse(content.replace(/^dydu\.(.*)\.callbacks\._([0-9]*)\(/, "").slice(0, -1)));
+            }
+        });
     }
 });
+
+function requestHandler(sent, response) {
+    const button = document.createElement('button');
+
+    button.innerText = history++;
+
+    button.addEventListener('click', (event) => {
+        const active = document.querySelector('.active');
+
+        if (active !== null)
+            active.classList.remove('active');
+
+        event.target.classList.add('active');
+
+        updateMonaco(decode(sent), decode(response));
+    }, false);
+
+    document.querySelector("#history").appendChild(button);
+
+    const buttons = document.querySelectorAll("#history button");
+
+    if (buttons.length === 1 || buttons[buttons.length - 2].classList.contains('active'))
+        buttons[buttons.length - 1].click();
+}
